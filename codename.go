d35ba2b1d3ecb0c78148b91f819ef624a9fc4a46
Saman Shahbazi <sammm.sh@gmail.com>
2025-04-30 19:31:34 +0200

    remove letters from additional chars added

--- codename.go
+++ codename.go
@@ -61,9 +61,7 @@ func randomToken(rng *rand.Rand, size int) string {
 	hex := []byte{
 		'0', '1', '2', '3',
 		'4', '5', '6', '7',
-		'8', '9', 'a', 'b',
-		'c', 'd', 'e', 'f',
-	}
+		'8', '9'}
 
 	var buffer bytes.Buffer
 
